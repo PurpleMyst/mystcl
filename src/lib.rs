ded9390a982eabc1ac92b67c161f281f15218339
Purple Myst <PurpleMyst@users.noreply.github.com>
2019-05-14 14:27:14 +0200

    feat: Add _tkinter.create

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, wrap_pyfunction};
+
+#[pyclass]
+struct TkApp {
+
+}
+
+impl TkApp {
+    fn new() -> Self {
+        TkApp {}
+    }
+}
+
+#[pyfunction]
+fn create() -> PyResult<Py<TkApp>> {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let obj = Py::new(py, TkApp::new())?;
+    Ok(obj)
+}
 
 #[pymodule]
-fn mystcl(_py: Python, _m: &PyModule) -> PyResult<()> {
+fn mystcl(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_wrapped(wrap_pyfunction!(create))?;
     Ok(())
 }
